c8eeb1e4e6707cc378229e1b1902c718878e157a
ImanliOmer <omarzi@code.edu.az>
2022-04-01 17:31:38 +0400

    first upload(davami gelecek)

---
+++ Generic-Collections1/Generic-Collections/Models/Group.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections.Models
+{
+    internal class Group
+    {
+        private int _no;
+        private static Student[] student;
+
+        public int No{ get; }
+        public Group(int no)
+        {
+            No = no;
+        }     
+        public Group()
+        {
+            No++;
+            _no=No;
+        }
+        public static Student[] createArray()
+        {
+
+
+            return student;
+
+        }
+
+
+    }
+}
---
+++ Generic-Collections1/Generic-Collections/Models/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections.Models
+{
+    internal class Student
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int Point { get; set; }
+
+        public Student(string name,string surname,int point)
+        {
+            Name = name;
+            Surname = surname;
+            Point = point;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id}\n" +
+                $"Name: {Name}\n" +
+                $"surname: {Surname}\n" +
+                $"Age: {Age}\n" +
+                $"Point: {Point}");
+        }
+
+        public static bool operator >(Student p1,Student p2)
+        {
+            return p1.Point > p2.Point;
+        }
+        public static bool operator <(Student p1, Student p2)
+        {
+            return p1.Point < p2.Point;
+        }
+
+
+       
+
+
+    }
+}
