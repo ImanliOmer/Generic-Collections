05c1323d7d8865ed224f770980bf32990828e5e6
ImanliOmer <omarzi@code.edu.az>
2022-04-04 16:26:53 +0400

    upload 2

---
+++ Generic-Collections1/Generic-Collections/ExxHelper/Extenction.cs
@@ -0,0 +1,27 @@
+﻿using Generic_Collections.Models;
+
+namespace Generic_Collections.ExxHelper
+{
+    static class Extensions
+    {
+        public static Student[] PointSort(this Student[] stu)
+        {
+            Student min_value = stu[0];
+            for (int i = 0; i < stu.Length; i++)
+            {
+                for (int j = i + 1; j < stu.Length; j++)
+                {
+                    if (stu[i] > stu[j])
+                    {
+                        min_value = stu[j];
+                        stu[j] = stu[i];
+                        stu[i] = min_value;
+                    }
+                }
+            }
+            return stu;
+
+        }
+    }
+
+}
--- Generic-Collections1/Generic-Collections/Models/Group.cs
+++ Generic-Collections1/Generic-Collections/Models/Group.cs
@@ -1,33 +1,63 @@
-﻿using System;
+﻿using Generic_Collections.ExxHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Generic_Collections.Models
 {
     internal class Group
     {
-        private int _no;
-        private static Student[] student;
+        private static int _groupNo = -1;
+        public string GroupNo { get; }
 
-        public int No{ get; }
-        public Group(int no)
+
+
+        Regex regex = new Regex(@"([a-zA-Z]+)(\d+)");
+
+        private Student[] students;
+        private Student[] students2;
+
+
+        public Group(string groupno)
         {
-            No = no;
-        }     
-        public Group()
-        {
-            No++;
-            _no=No;
+            students = new Student[0];
+            GroupNo = groupno;
+            _groupNo++;
+            Match result = regex.Match(GroupNo);
+            string word = result.Groups[1].Value;
+            int num = int.Parse(result.Groups[2].Value);
+            GroupNo = word + (_groupNo + num);
+
         }
-        public static Student[] createArray()
+
+
+        public void AddStudent(Student stu)
         {
-
-
-            return student;
-
+            Array.Resize(ref students, students.Length + 1);
+            students[students.Length - 1] = stu;
+            Console.WriteLine($"Student:{stu.Name} has been added to:{GroupNo} group !");
+            students2 = students;
+            return;
         }
+        public void GetAllStudents()
+        {
+            foreach (Student stu in students)
+            {
+                Console.WriteLine(stu);
+            }
+        }
+        public void Sort()
+        {
+            students2.PointSort();
+            foreach (Student stu2 in students2)
+            {
+                Console.WriteLine(stu2);
+            }
+        }
+
 
 
     }
