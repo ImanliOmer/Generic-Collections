455aee3163ce60b35bdaa62034127edd04491c8c
ImanliOmer <omarzi@code.edu.az>
2022-04-04 16:34:47 +0400

    upload 3

--- Generic-Collections1/Generic-Collections/Program.cs
+++ Generic-Collections1/Generic-Collections/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Generic_Collections.Models;
+using System;
 
 namespace Generic_Collections
 {
@@ -6,7 +7,23 @@ namespace Generic_Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student stu = new Student("Cabbarov", "Elnur", 17);
+            Student stu1 = new Student("Suleyman", "Suleymanli", 18);
+            Student stu2 = new Student("Huseyn", "Elizade", 18);
+            Student stu3 = new Student("Mehdi", "Mehdiyev", 18);
+
+            Group qrup = new Group("pp111");
+
+            qrup.AddStudent(stu);
+            qrup.AddStudent(stu1);
+            qrup.AddStudent(stu2);
+            qrup.AddStudent(stu3);
+
+            Console.WriteLine("\nTelebelerin siyahisi");
+            qrup.GetAllStudents();
+
+            Console.WriteLine("\nSort siyahisi");
+            qrup.Sort();
         }
     }
 }
